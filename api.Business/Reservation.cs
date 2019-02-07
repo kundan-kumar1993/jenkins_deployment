42493a0575fae7d133a6e2f585e34e4cde9d2259
Kundan Kumar Rai <kundan.rai@evry.com>
2019-02-07 15:38:03 +0530

    Added tests to api

---
+++ api.Business/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace api.Business
+{
+    public class Reservation
+    {
+        public bool CanMakeReservation(User user)
+        {
+            if (user.Role == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+    }
+
+    public class User
+    {
+        public int Role { get; set; }
+    }
+}
---
+++ api.Tests/ReservationTests.cs
@@ -0,0 +1,22 @@
+using api.Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace api.Tests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanMakeReservation_IsUserAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanMakeReservation(new User() { Role = 1 });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
